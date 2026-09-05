ae8bfae77aa7a355da0f836239e139521eed1633
AcidSepp <yannick.weber@haw-landshut.de>
2026-09-05 22:02:31 +0200

    implement switch for hold function

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <MIDI.h>
+#include <set>
+
+using namespace std;
 
 // !!!!The midi library uses the PIN Number not the GPIO Number!!!!
-const int MIDI_RX_PIN = 16;
-const int MIDI_TX_PIN = 17;
+constexpr int MIDI_RX_PIN = 16;
+constexpr int MIDI_TX_PIN = 17;
+constexpr int HOLD_ON_OFF_SWITCH_PIN = 22;
+constexpr byte CHANNEL = 1;
 
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
-void noteOn(byte channel, byte note, byte velocity);
-void noteOff(byte channel, byte note, byte velocity);
+static void noteOn(byte channel, byte note, byte velocity);
+static void noteOff(byte channel, byte note, byte velocity);
 
-int notesPressed = 0;
+static set<int> pressedNotes;
+static set<int> sustainedNotes;
+static bool holdFunctionActivated;
 
 void setup() {
-    Serial.begin(31250, SERIAL_8N1, MIDI_RX_PIN, MIDI_TX_PIN);
+    Serial.begin(9600);
+
+    Serial1.begin(31250, SERIAL_8N1, MIDI_RX_PIN, MIDI_TX_PIN);
     MIDI.begin(MIDI_CHANNEL_OMNI);
     MIDI.turnThruOff();
     MIDI.setHandleNoteOn(noteOn);
@@ -26,28 +35,76 @@ void setup() {
         delay(100);
         digitalWrite(LED_BUILTIN, LOW);
     }
+
+    pinMode(HOLD_ON_OFF_SWITCH_PIN, INPUT);
+    if (digitalRead(HOLD_ON_OFF_SWITCH_PIN) == HIGH) {
+        holdFunctionActivated = true;
+        digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+        holdFunctionActivated = false;
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 }
 
 void loop() {
+    const bool oldHoldFunctionState = holdFunctionActivated;
+    const bool newHoldFunctionState = digitalRead(HOLD_ON_OFF_SWITCH_PIN) == HIGH;
+
+    // hold function was off and is now on
+    if (!oldHoldFunctionState && newHoldFunctionState) {
+        holdFunctionActivated = true;
+        digitalWrite(LED_BUILTIN, HIGH);
+
+        Serial.println("Switching Hold function ON");
+
+        // add a little delay, to debounce the HOLD button
+        delay(50);
+    }
+
+    // hold function is on and is now turned off
+    if (oldHoldFunctionState && !newHoldFunctionState) {
+        holdFunctionActivated = false;
+        digitalWrite(LED_BUILTIN, LOW);
+
+        for (const int sustainedNote: sustainedNotes) {
+            if (!pressedNotes.count(sustainedNote)) {
+                Serial.printf("Sending Note OFF: %d\n", sustainedNote);
+                MIDI.sendNoteOff(sustainedNote, 0, CHANNEL);
+            } else {
+                Serial.printf("Not sending Note OFF: %d\n", sustainedNote);
+            }
+        }
+        sustainedNotes.clear();
+
+        Serial.println("Switching Hold function OFF");
+        // add a little delay, to debounce the HOLD button
+        delay(50);
+    }
+
     MIDI.read();
 }
 
 void noteOn(const byte channel, const byte note, const byte velocity) {
-    MIDI.sendNoteOn(note, velocity, channel);
+    MIDI.sendNoteOn(note, velocity, CHANNEL);
 
-    // this needs to be done after the new note is entered, otherwise the loop causes an input delay
-    if (notesPressed == 0) {
-        for (int i = 0; i < 127; i++) {
-            if (i != note) { // do not stop the new note
-                MIDI.sendNoteOff(i, velocity, channel);
+    // the user is entering a new chord
+    if (pressedNotes.empty()) {
+        for (const int sustainedNote: sustainedNotes)
+            if (sustainedNote != note) {
+                MIDI.sendNoteOff(sustainedNote, velocity, CHANNEL);
             }
-        }
+        sustainedNotes.clear();
     }
-    notesPressed++;
-    digitalWrite(LED_BUILTIN, HIGH);
+    pressedNotes.insert(note);
+    sustainedNotes.insert(note);
+
+    Serial.printf("Sustained Notes: %d\n", sustainedNotes.size());
 }
 
 void noteOff(const byte channel, const byte note, const byte velocity) {
-    notesPressed--;
-    digitalWrite(LED_BUILTIN, LOW);
+    if (!holdFunctionActivated) {
+        MIDI.sendNoteOff(note, velocity, CHANNEL);
+        sustainedNotes.erase(note);
+    }
+    pressedNotes.erase(note);
 }

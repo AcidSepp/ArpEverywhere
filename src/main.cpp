47e63329ca35b3e42efdd1eff3ae5d80da80150b
AcidSepp <yannick.weber@haw-landshut.de>
2026-09-17 23:31:26 +0200

    implement hold function on arduino nano (bugged)

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,70 @@
+#include <SoftwareSerial.h>
 #include <MIDI.h>
-#include <set>
-#include "rotarySwitch.h"
-#include "timedivision.h"
 
-using namespace std;
-
-// !!!!The midi library uses the PIN Number not the GPIO Number!!!!
-constexpr int MIDI_RX_PIN = 16;
-constexpr int MIDI_TX_PIN = 17;
-constexpr int HOLD_ON_OFF_SWITCH_PIN = 22;
-constexpr int ROTARY_SWITCH_PIN = 35;
+constexpr int MIDI_RX_PIN = 2;
+constexpr int MIDI_TX_PIN = 3;
+constexpr int HOLD_ON_OFF_SWITCH_PIN = 4;
 constexpr byte CHANNEL = 1;
-constexpr bool DEBUG = true;
 
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+SoftwareSerial midiSerial(MIDI_RX_PIN, MIDI_TX_PIN);
+
+MIDI_CREATE_INSTANCE(SoftwareSerial, midiSerial, MIDI);
 
 static void noteOn(byte channel, byte note, byte velocity);
 
 static void noteOff(byte channel, byte note, byte velocity);
 
-static void handleClock();
-
-static set<int> pressedNotes;
-static set<int> sustainedNotes;
+static bool pressedNotes[127];
+static bool sustainedNotes[127];
+static int pressedNotesCount = 0;
+static int sustainedNotesCount = 0;
 static bool holdFunctionActivated;
-static bool arpFunctionActivated = true;
-static int clockCounter = 0;
-static int lastArpNote = 0;
-static int arpIndex = 0;
-static TimeDivision pulsesPerNote = _1_4;
+
+static void printSustainedNotes() {
+    Serial.print("Sustained Notes:");
+    for (int i = 0; i < 127; i++) {
+        if (sustainedNotes[i]) {
+            Serial.print(i);
+            Serial.print(" ");
+        }
+    }
+    Serial.println();
+}
+
+static void printPressedNotes() {
+    Serial.print("Pressed Notes:");
+    for (int i = 0; i < 127; i++) {
+        if (pressedNotes[i]) {
+            Serial.print(i);
+            Serial.print(" ");
+        }
+    }
+    Serial.println();
+}
+
+static void noteOffForAllExceptPressed() {
+    for (int i = 0; i < 127; i++) {
+        if (!pressedNotes[i]) {
+            MIDI.sendNoteOff(i, 0, CHANNEL);
+        }
+        sustainedNotes[i] = false;
+    }
+    sustainedNotesCount = 0;
+}
 
 void setup() {
     Serial.begin(9600);
+    Serial.println("Starting");
 
-    Serial1.begin(31250, SERIAL_8N1, MIDI_RX_PIN, MIDI_TX_PIN);
+    pinMode(MIDI_RX_PIN, INPUT);
+    pinMode(MIDI_TX_PIN, OUTPUT);
+    midiSerial.begin(31250);
     MIDI.begin(MIDI_CHANNEL_OMNI);
     MIDI.turnThruOff();
     MIDI.setHandleNoteOn(noteOn);
     MIDI.setHandleNoteOff(noteOff);
-    MIDI.setHandleClock(handleClock);
+
+    noteOffForAllExceptPressed();
 
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
@@ -53,12 +79,12 @@ void setup() {
     if (digitalRead(HOLD_ON_OFF_SWITCH_PIN) == HIGH) {
         holdFunctionActivated = true;
         digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("Switching Hold function ON");
     } else {
         holdFunctionActivated = false;
         digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Switching Hold function OFF");
     }
-
-    pinMode(ROTARY_SWITCH_PIN, INPUT);
 }
 
 void loop() {
@@ -70,9 +96,7 @@ void loop() {
         holdFunctionActivated = true;
         digitalWrite(LED_BUILTIN, HIGH);
 
-        if (DEBUG) {
-            Serial.println("Switching Hold function ON");
-        }
+        Serial.println("Switching Hold function ON");
 
         // add a little delay, to debounce the HOLD button
         delay(50);
@@ -83,19 +107,12 @@ void loop() {
         holdFunctionActivated = false;
         digitalWrite(LED_BUILTIN, LOW);
 
-        for (const int sustainedNote: sustainedNotes) {
-            if (!pressedNotes.count(sustainedNote)) {
-                Serial.printf("Sending Note OFF: %d\n", sustainedNote);
-                MIDI.sendNoteOff(sustainedNote, 0, CHANNEL);
-            } else {
-                Serial.printf("Not sending Note OFF: %d\n", sustainedNote);
-            }
-        }
-        sustainedNotes.clear();
+        printSustainedNotes();
+        printPressedNotes();
 
-        if (DEBUG) {
-            Serial.println("Switching Hold function OFF");
-        }
+        noteOffForAllExceptPressed();
+
+        Serial.println("Switching Hold function OFF");
         // add a little delay, to debounce the HOLD button
         delay(50);
     }
@@ -104,78 +121,49 @@ void loop() {
 }
 
 void noteOn(const byte channel, const byte note, const byte velocity) {
-    // if the arp is active, the note will be played automatically, so we need to prevent retriggers in that case
-    // In very slow arp speeds it would take a long time until the note sounds, so we play the first not anyways.
-    if (!arpFunctionActivated || pressedNotes.empty()) {
-        MIDI.sendNoteOn(note, velocity, CHANNEL);
-    }
+    MIDI.sendNoteOn(note, velocity, CHANNEL);
 
     // the user is entering a new chord
-    if (pressedNotes.empty()) {
-        for (const int sustainedNote: sustainedNotes) {
-            if (sustainedNote != note) {
-                MIDI.sendNoteOff(sustainedNote, velocity, CHANNEL);
+    if (pressedNotesCount == 0) {
+        for (int i = 0; i < 127; i++) {
+            if (sustainedNotes[i] && note != i) {
+                Serial.print("Sending Note OFF: ");
+                Serial.println(i);
+                MIDI.sendNoteOff(i, 0, CHANNEL);
             }
+            sustainedNotes[i] = false;
         }
-        sustainedNotes.clear();
+        sustainedNotesCount = 0;
     }
-    pressedNotes.insert(note);
-    sustainedNotes.insert(note);
+    pressedNotes[note] = true;
+    sustainedNotes[note] = true;
+    sustainedNotesCount++;
+    pressedNotesCount++;
 
-    if (DEBUG) {
-        Serial.printf("Sustained Notes: %d\n", sustainedNotes.size());
-    }
+    Serial.print("Sustained Notes: ");
+    Serial.println(sustainedNotesCount);
+    Serial.print("Pressed Notes: ");
+    Serial.println(pressedNotesCount);
 }
 
 void noteOff(const byte channel, const byte note, const byte velocity) {
     if (!holdFunctionActivated) {
         MIDI.sendNoteOff(note, velocity, CHANNEL);
-        sustainedNotes.erase(note);
+        sustainedNotes[note] = true;
+        sustainedNotesCount--;
     }
-    pressedNotes.erase(note);
-}
+    pressedNotes[note] = false;
+    pressedNotesCount--;
 
-static void handleClock() {
-    if (clockCounter % pulsesPerNote == 0) {
-        if (DEBUG) {
-            Serial.println("Quarter Note!");
-            Serial.printf("Sustained Notes: %d\n", sustainedNotes.size());
-        }
+    Serial.print("Sustained Notes: ");
+    Serial.println(sustainedNotesCount);
+    Serial.print("Pressed Notes: ");
+    Serial.println(pressedNotesCount);
 
-        if (!sustainedNotes.empty()) {
-            if (arpIndex >= sustainedNotes.size()) {
-                arpIndex = 0;
-            }
-
-            Serial.printf("arpIndex: %d\n", arpIndex);
-
-            int index = 0;
-            for (const int sustainedNote: sustainedNotes) {
-                // we send all note offs here, to avoid stuck notes, when arp is switched on
-                // Note Off needs to go first, in order to retrigger the note, if it is the only one sustained
-                MIDI.sendNoteOff(lastArpNote, 127, CHANNEL);
-
-                if (index == arpIndex) {
-                    MIDI.sendNoteOn(sustainedNote, 127, CHANNEL);
-                    lastArpNote = sustainedNote;
-                }
-                index++;
-            }
-
-            arpIndex++;
-        }
+    if (pressedNotesCount < 0) {
+        pressedNotesCount = 0;
     }
-
-    // Read the rotary switch every 1/4 note, this should suffice in accuracy
-    if (clockCounter % _1_4 == 0) {
-        const int sensorValue = analogRead(ROTARY_SWITCH_PIN);
-        pulsesPerNote = rotarySwitchNumberToSubdivision(getRotarySwitchNumber(sensorValue));
-
-        if (DEBUG) {
-            Serial.printf("pulsesPerNote: %d\n", pulsesPerNote);
-        }
+    if (sustainedNotesCount < 0) {
+        sustainedNotesCount = 0;
     }
-
-    // the clock counter needs to stay in range between 0 and 32 quarter notes, as this is the biggest subdivision we support
-    clockCounter = (clockCounter + 1) % _32_4;
 }
